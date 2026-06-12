b792ab4e1ec4adb2ff7bd0279416f7d74db8b0bc
Soumya-majhi-hub <alok140805@gmail.com>
2026-06-12 22:55:19 +0530

    config: synchronize security properties configuration settings with H2 parameters

---
+++ ecom-proj/src/main/java/com/black/ecom_proj/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.black.ecom_proj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http)throws Exception {
+        http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request
+                .requestMatchers("/api/products","/api/products/**","/index.html").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+    return http.build();
+    }
+}
